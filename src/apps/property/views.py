ccb230121387141fa858cc1d855a0123c5482558
marceloagoston <marceloagoston91@gmail.com>
2024-04-02 14:22:32 -0300

    feat: custom pagination properties

--- src/apps/property/views.py
+++ src/apps/property/views.py
@@ -1,5 +1,7 @@
 from rest_framework import filters, viewsets
 
+from apps.core.paginator import CustomPagination
+
 from .models import Property
 from .serializers import PropertySerializer
 
@@ -9,3 +11,18 @@ class PropertyApiView(viewsets.ReadOnlyModelViewSet):
     queryset = Property.objects.all().order_by("name")
     filter_backends = [filters.SearchFilter]
     search_fields = ["name"]
+    pagination_class = CustomPagination
+
+    def get_pagination_class(self):
+        """Determinar si usar o no paginación
+        - page_size = valor
+        - valor = un numero entero, será el tamaño de la pagina
+        - valor = none, no se pagina el resultado
+        """
+        if self.request.GET.get("page_size") == "none":
+            return None
+
+        return self.pagination_class
+    def list(self, request, *args, **kwargs):
+        self.pagination_class = self.get_pagination_class()
+        return super().list(request, *args, **kwargs)
