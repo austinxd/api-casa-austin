b82e426f6fc95f687bebf5b8419be8e2d2a88389
marceloagoston <marceloagoston91@gmail.com>
2024-04-07 01:12:57 -0300

    feat: fix warnings dashboard

--- src/apps/dashboard/views.py
+++ src/apps/dashboard/views.py
@@ -1,4 +1,6 @@
-from datetime import datetime, timedelta
+from django.utils import timezone
+
+from datetime import timedelta
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -15,17 +17,26 @@ class DashboardApiView(APIView):
     def get(self, request):
         content = {}
 
-        week = datetime.now() - timedelta(days=1)
-        reservations_week = Reservation.objects.filter(created__gte=week, created__lte=datetime.now()).count()
+        current_datetime = timezone.now()
+        week = current_datetime - timedelta(days=1)
+        reservations_week = Reservation.objects.filter(created__gte=week, created__lte=current_datetime).count()
         properties_more_reserved = Reservation.objects.filter(
-            created__gte=week,
-            created__lte=datetime.now()
-        ).values('property').annotate(num_reservas=Count('id')).order_by('-num_reservas')[:4]
-        content['properties_more_reserved'] = properties_more_reserved.annotate(
-            percentage=ExpressionWrapper((F('num_reservas') * 100) / reservations_week, output_field=DecimalField())
-        )
+                created__gte=week,
+                created__lte=current_datetime
+            ).values(
+                'property',
+                "property__background_color"
+            ).annotate(
+                num_reservas=Count('id'),
+            ).order_by('-num_reservas')
 
-        reservations_week = Reservation.objects.filter(created__gte=week, created__lte=datetime.now()).count()
+        content['properties_more_reserved'] = properties_more_reserved.annotate(
+            percentage=ExpressionWrapper(
+                (F('num_reservas') * 100) / reservations_week, output_field=DecimalField()
+                )
+            )
+
+        reservations_week = Reservation.objects.filter(created__gte=week, created__lte=current_datetime).count()
         content["reservations_last_week"] = reservations_week
 
         return Response(content, status=200)
