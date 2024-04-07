11108cd2797f8ebb887899cb0fb7706cfb313fb2
marceloagoston <marceloagoston91@gmail.com>
2024-04-07 10:36:01 -0300

    add info seller in return daashboard

--- src/apps/dashboard/views.py
+++ src/apps/dashboard/views.py
@@ -5,6 +5,7 @@ from datetime import timedelta
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from apps.accounts.models import CustomUser
 from apps.property.models import Property
 from apps.reservation.models import Reservation
 
@@ -21,7 +22,7 @@ class DashboardApiView(APIView):
         current_datetime = timezone.now()
         week = current_datetime - timedelta(days=1)
         reservations_week = Reservation.objects.filter(created__gte=week, created__lte=current_datetime).count()
-
+        reservations_week_seller = Reservation.objects.filter(created__gte=week, created__lte=current_datetime, seller__isnull=False).count()
         """" Propiedades """
         properties_more_reserved = Reservation.objects.filter(
                 created__gte=week,
@@ -63,16 +64,53 @@ class DashboardApiView(APIView):
         content['properties_more_reserved'] = list(content['properties_more_reserved']) + list_properties_without_reserved
 
         # """" Vendedores """
-        # seller_more_reserved = Reservation.objects.filter(
-        #         created__gte=week,
-        #         created__lte=current_datetime
-        #     ).values(
-        #         'property',
-        #         'property__background_color',
-        #         'property__name',
-        #     ).annotate(
-        #         num_reservas=Count('id'),
-        #     ).order_by('-num_reservas')
+        seller_more_reserved = Reservation.objects.filter(
+                created__gte=week,
+                created__lte=current_datetime,
+                seller__isnull=False,
+            ).values(
+                'seller',
+                'seller__email',
+                'seller__last_name',
+                'seller__first_name',
+            ).annotate(
+                num_reservas=Count('id'),
+            ).order_by('-num_reservas')
+        
+        print('\n\n\n')
+        print('SELERRRRSS ', seller_more_reserved)
+        print('\n\n\n')
+        
+        # Agregar propiedades que no tienen reservas
+        seller_list = Reservation.objects.filter(
+                created__gte=week,
+                created__lte=current_datetime,
+                seller__isnull=False,
+            ).values_list(
+                'seller',
+                flat=True
+            )
+
+        list_seller_without_reserved = []
+        seller_without_reserved = CustomUser.objects.exclude(id__in=seller_list)
+        for s in seller_without_reserved:
+            dict_aux = {
+                'seller': s.id,
+                'seller__email': s.email,
+                'seller__last_name': s.last_name,
+                'seller__first_name': s.first_name,
+                'num_reservas': 0,
+                'percentage': float(0)
+            }
+            list_seller_without_reserved.append(dict_aux)
+
+        content['seller_more_reserved'] = seller_more_reserved.annotate(
+            percentage=ExpressionWrapper(
+                (F('num_reservas') * 100) / reservations_week_seller, output_field=DecimalField()
+                )
+            )
+
+        content['seller_more_reserved'] = list(content['seller_more_reserved']) + list_seller_without_reserved
 
         reservations_week = Reservation.objects.filter(created__gte=week, created__lte=current_datetime).count()
         content["reservations_last_week"] = reservations_week
