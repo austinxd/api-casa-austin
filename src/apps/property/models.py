03bf05394ed15b53a3a819298afe59ac6fd12a36
marceloagoston <marceloagoston91@gmail.com>
2024-04-18 11:14:04 -0300

    add fields url pool tempreature in properties

--- src/apps/property/models.py
+++ src/apps/property/models.py
@@ -11,6 +11,8 @@ class Property(BaseModel):
     airbnb_url = models.URLField(null=True, blank=True)
     capacity_max = models.IntegerField(null=True, blank=True)
     background_color = models.CharField(max_length=255, null=False, blank=False, default="#fff")
+    on_temperature_pool_url = models.URLField(null=True, blank=True)
+    off_temperature_pool_url = models.URLField(null=True, blank=True)
 
     def __str__(self):
         return self.name
