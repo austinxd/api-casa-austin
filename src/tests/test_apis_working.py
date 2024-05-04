a8c25ac6b2dd86b26c527bc6a43580ca52b0e513
marceloagoston <marceloagoston91@gmail.com>
2024-05-04 10:47:54 -0300

    agregar utils en tests; agregar test general de toda la api; mandar a wip a test de reservas

---
+++ src/tests/test_apis_working.py
@@ -0,0 +1,42 @@
+from django.test import TestCase
+from rest_framework.test import APITestCase, APIClient
+
+from django.urls import reverse
+
+from tests.utils.functions import create_group, create_user
+
+
+class APIGeneralTest(APITestCase):
+    """ Testear respuestas de todas las apis LISTView
+    - Desc: Probar que algun cambio no haya afectado al funcionamiento general de otras apis.
+    La sola respuesta 200 indica que todo funciona a grandes razgos
+    """
+    def setUp(self):
+        group_instance = create_group("admin")
+        self.user = create_user(True, group_instance)
+
+        self.client.force_authenticate(user=self.user)
+
+    def test_dashboard_response(self):
+        response = self.client.get(reverse('dashboard'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_token_rutificador_response(self):
+        response = self.client.get(reverse('token_rutificador'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual("token" in response.data, True)
+
+    def test_property_response(self):
+        url = reverse('property-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_clients_response(self):
+        url = reverse('clients-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_reservation_response(self):
+        url = reverse('reservations-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
