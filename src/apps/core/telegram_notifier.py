70006a8d8043ef7838da7b0882554f5f91ed9c29
Augusto Tantalean <austin.xd@live.com>
2024-06-13 20:41:42 -0500

    notificaciones Telegram13

--- src/apps/core/telegram_notifier.py
+++ src/apps/core/telegram_notifier.py
@@ -2,6 +2,7 @@ import os
 import asyncio
 import logging
 from telegram import Bot
+from telegram.error import TelegramError
 from concurrent.futures import ThreadPoolExecutor
 
 logger = logging.getLogger('apps')
@@ -22,9 +23,12 @@ def run_async(func):
 
 @run_async
 async def async_send_telegram_message(message):
-    logger.debug("Enviando mensaje asincrónicamente a Telegram.")
-    await bot.send_message(chat_id=CHAT_ID, text=message)
-    logger.debug("Mensaje enviado.")
+    try:
+        logger.debug("Enviando mensaje asincrónicamente a Telegram.")
+        await bot.send_message(chat_id=CHAT_ID, text=message)
+        logger.debug("Mensaje enviado.")
+    except TelegramError as e:
+        logger.error(f"Error enviando mensaje a Telegram: {e}")
 
 def send_telegram_message(message):
     logger.debug(f"Preparando para enviar mensaje: {message}")
