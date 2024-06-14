571919f541a05aa6a9fcb6aeb5c2605f88ead95e
Augusto Tantalean <austin.xd@live.com>
2024-06-13 19:57:58 -0500

    notificaciones Telegram1

--- src/apps/reservation/signals.py
+++ src/apps/reservation/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Reservation
-from src.apps.core.telegram_notifier import send_telegram_message
+from ..core.telegram_notifier import send_telegram_message
 
 def notify_new_reservation(reservation):
     message = f"Se ha creado una nueva reserva:\nID: {reservation.id}\nCliente: {reservation.cliente}\nFecha de check-in: {reservation.check_in_date}"
