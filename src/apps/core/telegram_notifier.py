671679364149a36de2a2d0b021ff8a43b4e53a51
Augusto Tantalean <austin.xd@live.com>
2024-06-13 20:08:36 -0500

    notificaciones Telegram3

--- src/apps/core/telegram_notifier.py
+++ src/apps/core/telegram_notifier.py
@@ -1,10 +1,14 @@
 import os
+import asyncio
 from telegram import Bot
 
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-CHAT_ID = os.getenv('CHAT_ID')
+CHAT_ID = 'your-chat-id'  # Reemplaza esto con el ID del chat donde quieres recibir las notificaciones
 
 bot = Bot(token=TELEGRAM_BOT_TOKEN)
 
+async def async_send_telegram_message(message):
+    await bot.send_message(chat_id=CHAT_ID, text=message)
+
 def send_telegram_message(message):
-    bot.send_message(chat_id=CHAT_ID, text=message)
+    asyncio.run(async_send_telegram_message(message))
