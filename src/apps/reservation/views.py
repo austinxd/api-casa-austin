34c4194b23dee312b4d6875b08583882b729fc24
Augusto Tantalean <austin.xd@live.com>
2024-07-16 15:11:22 -0500

    nuevo_endp2

--- src/apps/reservation/views.py
+++ src/apps/reservation/views.py
@@ -330,15 +330,227 @@ class ProfitApiView(APIView):
             status=200
         )
 
-class VistaCalendarioApiView(viewsets.ReadOnlyModelViewSet):
-    """
-    API endpoint que permite obtener reservaciones sin paginación para uso en calendarios.
-    """
-    queryset = Reservation.objects.all()
+############ Austin MOD ############
+class VistaCalendarioApiView(viewsets.ModelViewSet):
     serializer_class = ReservationSerializer
-    permission_classes = [AllowAny]  # Ajusta según tus requisitos de seguridad
+    queryset = Reservation.objects.exclude(deleted=True).order_by("check_in_date")
+    search_fields = [
+        "client__email",
+        "client__first_name",
+        "client__last_name",
+        "property__name"
+    ]
     pagination_class = None  # Desactiva la paginación
 
     def get_queryset(self):
-        # Aquí puedes ajustar la consulta si necesitas filtrar los resultados
-        return super().get_queryset()
+        queryset = super().get_queryset()
+
+        if self.action == 'list':
+            if self.request.query_params:
+                from_param = self.request.query_params.get('from')
+                type_param = self.request.query_params.get('type')
+                now = datetime.now()
+                check_in_time = time(15, 0)  # 3 PM
+                check_out_time = time(11, 0)  # 11 AM
+
+                if self.request.query_params.get('year') and self.request.query_params.get('month'):
+                    try:
+                        month_param = int(self.request.query_params['month'])
+                        if not month_param in range(1, 13):
+                            raise ValidationError({"error": "Parámetro Mes debe ser un número entre el 1 y el 12"})
+                    except Exception:
+                        raise ValidationError({"error_month_param": "Parámetro Mes debe ser un número entre el 1 y el 12"})
+
+                    try:
+                        year_param = int(self.request.query_params['year'])
+                        if year_param < 1:
+                            raise ValidationError({"error": "Parámetro Año debe ser un número entero positivo"})
+                    except Exception:
+                        raise ValidationError({"error_year_param": "Año debe ser un número entero positivo"})
+
+                    last_day_month = calendar.monthrange(year_param, month_param)[1]
+
+                    range_evaluate = (datetime(year_param, month_param, 1), datetime(year_param, month_param, last_day_month))
+
+                    if self.request.query_params.get('from_check_in') == 'true':
+                        queryset = queryset.filter(check_in_date__range=range_evaluate)
+                    else:
+                        queryset = queryset.filter(
+                            Q(check_in_date__range=range_evaluate) |
+                            Q(check_out_date__range=range_evaluate)
+                        )
+
+                if from_param == 'today':
+                    queryset = queryset.filter(check_in_date__gte=now)
+                elif from_param == 'in_progress':
+                    now_time = now.time()
+                    today_date = now.date()
+                    tomorrow_date = today_date + timedelta(days=1)
+
+                    queryset = queryset.filter(
+                        Q(check_in_date__lt=today_date, check_out_date__gt=today_date) |
+                        (Q(check_in_date=today_date) & Q(check_out_date__gt=today_date)) |
+                        (Q(check_out_date=today_date) & Q(check_out_date__gt=now)) |
+                        (Q(check_out_date=tomorrow_date) & Q(check_out_date__gt=datetime.combine(tomorrow_date, check_out_time)))
+                    )
+
+                if type_param:
+                    queryset = queryset.filter(origin=type_param)
+
+        elif self.action in ['partial_update', 'update', 'destroy']:
+            if not check_user_has_rol("admin", self.request.user):
+                queryset = queryset.filter(
+                    Q(origin="air") |
+                    Q(seller=self.request.user)
+                )
+
+        if self.request.query_params.get('exclude'):
+            queryset = queryset.exclude(origin=self.request.query_params['exclude'])
+
+        return queryset.exclude(deleted=True)
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return ReservationRetrieveSerializer
+        if self.action == 'list':
+            return ReservationListSerializer
+
+        return super().get_serializer_class()
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        if self.action == 'retrieve':
+            context['retrieve'] = True
+        return context
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "year",
+                OpenApiTypes.INT,
+                required=False,
+                description="Filter results by year",
+                enum=[2024, 2023, 2022],
+            ),
+            OpenApiParameter(
+                "month",
+                OpenApiTypes.INT,
+                required=False,
+                description="Filter results by month (number 1 to 12)",
+                enum=list(range(1, 13)),
+            ),
+            OpenApiParameter(
+                "page_size",
+                OpenApiTypes.INT,
+                description="Enviar page_size=valor para determinar tamaño de la pagina, sino enviar page_size=none para no tener paginado",
+                required=False,
+            ),
+            OpenApiParameter(
+                "from",
+                OpenApiTypes.STR,
+                description="Obtiene todas las reservas desde la fecha de hoy en adelante. Se puede combinar con type (tipo de reserva), pero no con year o month",
+                required=False,
+                enum=["today", "in_progress"]
+            ),
+            OpenApiParameter(
+                "from_check_in",
+                OpenApiTypes.STR,
+                description="Obtiene todas las reservas que hayan comenzado este mes. Se usa en combinacion de los queryparams year y month",
+                required=False,
+                enum=["true"]
+            ),
+            OpenApiParameter(
+                "type",
+                OpenApiTypes.STR,
+                description="Filtra las resevas según donde se generaron, AriBnB (air), Sistema Casa Austin (aus), Mantenimiento (man)",
+                required=False,
+                enum=["aus", "air", "man"]
+            ),
+            OpenApiParameter(
+                "exclude",
+                OpenApiTypes.STR,
+                description="Excluye las resevas según donde se generaron, AriBnB (air), Sistema Casa Austin (aus), Mantenimiento (man)",
+                required=False,
+                enum=["aus", "air", "man"]
+            ),
+        ],
+        responses={200: ReservationListSerializer(many=True)},
+        methods=["GET"],
+    )
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def perform_create(self, serializer):
+        with transaction.atomic():
+            user_seller = self.request.user
+            if self.request.POST['origin'].lower() == 'air':
+                user_seller = CustomUser.objects.get(first_name='AirBnB')
+
+            serializer.save(seller=user_seller)
+
+            for file in self.request.FILES.getlist('file'):
+                RentalReceipt.objects.create(
+                    reservation=serializer.instance,
+                    file=file
+                )
+
+        confeccion_ics()
+
+        generate_audit(
+            serializer.instance,
+            self.request.user,
+            "create",
+            "Reserva creada"
+        )
+
+    def perform_update(self, serializer):
+        confeccion_ics()
+
+        generate_audit(
+            serializer.instance,
+            self.request.user,
+            "update",
+            "Reserva actualizada full"
+        )
+
+        return super().perform_update(serializer)
+
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+
+        with transaction.atomic():
+            self.perform_update(serializer)
+
+            for file in request.FILES.getlist('file'):
+                RentalReceipt.objects.create(
+                    reservation=instance,
+                    file=file
+                )
+
+        confeccion_ics()
+
+        generate_audit(
+            serializer.instance,
+            self.request.user,
+            "update",
+            "Reserva actualizada"
+        )
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+
+        confeccion_ics()
+
+        generate_audit(
+            instance,
+            self.request.user,
+            "delete",
+            "Reserva eliminada"
+        )
+        return Response(status=204)
