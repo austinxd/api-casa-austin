5e6e578b9e52739391eb9bd29cbacb9e6ec1aac4
Augusto Tantalean <austin.xd@live.com>
2024-07-18 13:52:18 -0500

    endpoint_contractos12

--- src/apps/reservation/views.py
+++ src/apps/reservation/views.py
@@ -274,7 +274,7 @@ class ReservationsApiView(viewsets.ModelViewSet):
     def contrato(self, request, pk=None):
         try:
             reservation = self.get_object()
-            client = Clients.objects.get(id=reservation.client_id)  # Usa Clients aquí
+            client = Clients.objects.get(id=reservation.client_id)
             property = Property.objects.get(id=reservation.property_id)
 
             doc = DocxTemplate("/srv/casaaustin/api-casa-austin/src/plantilla.docx")
