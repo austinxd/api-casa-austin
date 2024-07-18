3c6d126961269faf767e451ea1b807d93a6f45ba
Augusto Tantalean <austin.xd@live.com>
2024-07-18 13:10:34 -0500

    endpoint_contractos5

--- src/apps/reservation/views.py
+++ src/apps/reservation/views.py
@@ -9,6 +9,7 @@ from django.db import transaction
 from django.db.models import Q
 
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from rest_framework import generics, viewsets
 from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
