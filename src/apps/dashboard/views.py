754cf7b0ca96817ffff9ce222fb895954a9ac6d4
Augusto Tantalean <austin.xd@live.com>
2024-07-20 19:44:28 -0500

    dashboard_get3

--- src/apps/dashboard/views.py
+++ src/apps/dashboard/views.py
@@ -21,40 +21,28 @@ class DashboardApiView(APIView):
         
         base_url = request.scheme + '://' + request.get_host()
         media_url = base_url + "/media/"
-        
-        # Obtener mes y año de los parámetros de la solicitud
-        month = request.query_params.get('month')
-        year = request.query_params.get('year')
-        
-        # Usar el mes y año actuales si no se proporcionan parámetros
-        if month and year:
-            month = int(month)
-            year = int(year)
-            is_current_month = False
-        else:
-            fecha_actual = datetime.now()
-            month = fecha_actual.month
-            year = fecha_actual.year
-            is_current_month = True
-        
-        last_day_month = calendar.monthrange(year, month)[1]
-        range_evaluate = (datetime(year, month, 1), datetime(year, month, last_day_month))
-        
+
+        # Best Sellers Card
+        fecha_actual = datetime.now()
+
+        last_day_month = calendar.monthrange(fecha_actual.year, fecha_actual.month)[1]
+
+        range_evaluate = (datetime(fecha_actual.year, fecha_actual.month, 1), datetime(fecha_actual.year, fecha_actual.month, last_day_month))
         query_reservation_current_month = Reservation.objects.exclude(deleted=True).filter(check_in_date__range=range_evaluate)
         
         best_sellers = []
         query_vendedores = CustomUser.objects.filter(
             Q(groups__name='vendedor') | Q(groups__name='admin')
         )
-        
+
         for v in query_vendedores:
             total_ventas_mes_vendedor = query_reservation_current_month.filter(seller=v).aggregate(total_ventas=Sum('price_sol'))
-            
+
             if total_ventas_mes_vendedor['total_ventas'] is None:
                 total_ventas_mes_vendedor = 0
             else:
                 total_ventas_mes_vendedor = '%.2f' % float(total_ventas_mes_vendedor['total_ventas'])
-            
+
             best_sellers.append({
                 'id': v.id,
                 'nombre': v.first_name,
@@ -62,19 +50,22 @@ class DashboardApiView(APIView):
                 'ventas_soles': total_ventas_mes_vendedor,
                 'foto_perfil': media_url+str(v.profile_photo) if v.profile_photo else base_url+'/static/default-user.jpg'
             })
-        
+
         content['best_sellers'] = best_sellers
-        
+
+        # END Best Sellers Card
+
         # Free days
-        free_days_per_house, free_days_total, ocuppied_days_total, total_por_cobrar, total_facturado = get_stadistics_period(year, month, last_day_month, is_current_month)
-        
+        free_days_per_house, free_days_total, ocuppied_days_total, total_por_cobrar, total_facturado = get_stadistics_period(fecha_actual, last_day_month)
+
         content['free_days_per_house'] = free_days_per_house
         content['free_days_total'] = free_days_total
         content['ocuppied_days_total'] = ocuppied_days_total
-        
+
         content['dinero_por_cobrar'] = total_por_cobrar
         content['dinero_total_facturado'] = total_facturado
-        
+
+
         # End Free days
-        
+
         return Response(content, status=200)
