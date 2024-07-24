988bb191d51f8bc21d1a03591d7ef7c3c97b2753
Augusto Tantalean <austin.xd@live.com>
2024-07-23 21:06:21 -0500

    dash19

--- src/apps/dashboard/utils.py
+++ src/apps/dashboard/utils.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 from apps.property.models import Property, ProfitPropertyAirBnb
 from apps.reservation.models import Reservation
 
