dee8569a63392cf448b48bb802b0c6b5fb7bd744
Augusto Tantalean <austin.xd@live.com>
2024-12-27 00:33:38 -0500

    airbnb1

--- src/02_script_airbnb_reservations.py
+++ src/02_script_airbnb_reservations.py
@@ -1,50 +1,30 @@
 import os
 import django
-import logging
-import pdb  # Para depuración interactiva
 
-# Configuración del entorno de Django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')  # Ajusta si es necesario
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')  # Ajusta el nombre si es necesario
 django.setup()  # Inicializa Django y carga las aplicaciones
 
-# Importación de funciones y modelos
 from apps.core.functions import confeccion_ics
 from apps.property.models import Property
+
 from apps.core.functions import update_air_bnb_api
 
-# Configuración de logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def get_airbnb_reservations():
-    logging.info('Comenzando proceso para obtener propiedades de API de AirBnB')
-    
-    # Consultar propiedades con URL de Airbnb
+    print('Comenzando proceso para obtener propiedades de API de AirBnB')
     query_property = Property.objects.exclude(airbnb_url__isnull=True)
-    logging.debug(f'Número de propiedades encontradas: {query_property.count()}')
 
     for q in query_property:
         try:
             if q.airbnb_url:
-                # Registro de la propiedad procesada
-                logging.debug(f'Procesando propiedad con ID: {q.id}, Nombre: {q.name}, URL: {q.airbnb_url}')
-                
-                # Depuración interactiva para inspeccionar
-                pdb.set_trace()  # Detiene la ejecución aquí
-                
-                # Actualización de la propiedad a través de la API
                 update_air_bnb_api(q)
         except Exception as e:
-            logging.error(f'Error obteniendo datos de API Airbnb para la propiedad {q.id}: {e}')
+            print('Error obteniendo datos api airbnb:', str(e))
 
-    logging.info('Finalizando proceso para obtener propiedades de API de AirBnB')
+    print('Finalizando proceso para obtener propeiedades de API de AirBnB')
 
 if __name__ == "__main__":
-    # Ejecutar sincronización con Airbnb
     get_airbnb_reservations()
-
-    # Actualizar calendario ICS después de la sincronización
-    try:
-        logging.info('Iniciando actualización de calendario ICS')
-        confeccion_ics()
-    except Exception as e:
-        logging.error(f'Error durante la actualización del calendario ICS: {e}')
+    # Llamar a confeccion_ics para actualizar el calendario ICS después de ejecutar el script
+    confeccion_ics()
