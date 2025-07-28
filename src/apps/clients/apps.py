d1da1108e6e064d12eccd93d1bd72a3c4fcd5e69
Augusto Tantalean <austin.xd@live.com>
2025-07-28 14:39:46 +0200

    paswd19

--- src/apps/clients/apps.py
+++ src/apps/clients/apps.py
@@ -5,10 +5,5 @@ class ClientsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.clients'
 
-
-class ClientsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.clients'
-
     def ready(self):
         import apps.clients.signals  # Asegúrate de que esto cargue las señales al iniciar
