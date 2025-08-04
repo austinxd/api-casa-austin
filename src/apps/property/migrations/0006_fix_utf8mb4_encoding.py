29376b3bc32c3b8653aff7a5529ec2aba90406e0
Augusto Tantalean <austin.xd@live.com>
2025-08-04 08:58:41 +0200

    caracteristicas property4

--- src/apps/property/migrations/0006_fix_utf8mb4_encoding.py
+++ src/apps/property/migrations/0006_fix_utf8mb4_encoding.py
@@ -1,4 +1,3 @@
-
 # Generated manually to fix charset encoding
 
 from django.db import migrations
@@ -12,7 +11,17 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RunSQL(
-            "ALTER TABLE property_property CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;",
-            reverse_sql="ALTER TABLE property_property CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
+            """
+            SET FOREIGN_KEY_CHECKS = 0;
+            ALTER TABLE property_property CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+            ALTER TABLE property_profitpropertyairbnb CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+            SET FOREIGN_KEY_CHECKS = 1;
+            """,
+            reverse_sql="""
+            SET FOREIGN_KEY_CHECKS = 0;
+            ALTER TABLE property_property CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+            ALTER TABLE property_profitpropertyairbnb CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+            SET FOREIGN_KEY_CHECKS = 1;
+            """
         ),
     ]
