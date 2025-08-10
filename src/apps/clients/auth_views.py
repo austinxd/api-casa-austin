19c56e55b986187ef402c95e4a389d5a993003c0
Augusto Tantalean <austin.xd@live.com>
2025-08-10 11:32:36 +0200

    endpoint track-search3

--- src/apps/clients/auth_views.py
+++ src/apps/clients/auth_views.py
@@ -398,9 +398,26 @@ class ClientCompleteRegistrationView(APIView):
 # Custom JWT Authentication for Clients
 class ClientJWTAuthentication(JWTAuthentication):
 
-    def get_user(self, validated_token):
+    def authenticate(self, request):
         """
-        Override to get client instead of user
+        Override authenticate method to handle client authentication
+        """
+        header = self.get_header(request)
+        if header is None:
+            return None
+
+        raw_token = self.get_raw_token(header)
+        if raw_token is None:
+            return None
+
+        validated_token = self.get_validated_token(raw_token)
+        client = self.get_client(validated_token)
+        
+        return (client, validated_token)
+
+    def get_client(self, validated_token):
+        """
+        Get client from validated token
         """
         try:
             # Intentar primero con client_id (nuestro claim personalizado)
@@ -414,6 +431,12 @@ class ClientJWTAuthentication(JWTAuthentication):
         except Clients.DoesNotExist:
             pass
         return None
+
+    def get_user(self, validated_token):
+        """
+        Override to get client instead of user (for compatibility)
+        """
+        return self.get_client(validated_token)
 
 
 class ClientVerifyDocumentView(APIView):
