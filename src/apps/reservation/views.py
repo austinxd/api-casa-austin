2fe033833a63a6937b44d137dabc2cc8c6bf801f
Augusto Tantalean <austin.xd@live.com>
2025-12-22 23:09:33 +0100

    front token active xx

--- src/apps/reservation/views.py
+++ src/apps/reservation/views.py
@@ -11,7 +11,7 @@ from django.db.models import Q
 
 from rest_framework.views import APIView
 from rest_framework.decorators import action
-from rest_framework import generics, viewsets, serializers
+from rest_framework import generics, viewsets, serializers, status
 from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import AllowAny, IsAuthenticated
